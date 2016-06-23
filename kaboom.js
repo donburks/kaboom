c85a885a9114f86c62233562eae541d58725632c
Don Burks <don@donburks.com>
2016-06-23 11:14:50 -0700

    Created bomb.html file with game in it, changed index to be panes of instructions that participants can page through. Updated CSS.

--- kaboom.js
+++ kaboom.js
@@ -1,18 +1,8 @@
-$(function() {
-  var total = 300000;
-
-  function updateTimer() {
-    var clock = utils.clockString(total);
-    if (clock == "0:00") {
-      utils.kaboom();
-      clearInterval(loop);
-      return false;
-    }
+updateTimer = function() {
+  var clock = utils.clockString(total);
+  if (clock == "0:00") {
+    utils.kaboom();
+  } else {
     $("#timer").text(clock);
   }
-
-  var loop = setInterval(function() {
-    total = total - 1000;
-    updateTimer();
-  }, 1000);
-});
+}
